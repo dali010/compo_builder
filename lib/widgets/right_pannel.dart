e12d146c88fa3f8427062002a793ed9bdf24601b
tahe <taha.bensaid132002@gmail.com>
2024-10-24 12:08:55 +0100

    fix text field like flutter flow design

--- lib/widgets/right_pannel.dart
+++ lib/widgets/right_pannel.dart
@@ -11,14 +11,14 @@ class RightPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      padding: const EdgeInsets.all(12),
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
       decoration: BoxDecoration(
-        color: const Color(0xFFFAFAFA),
-        borderRadius: BorderRadius.circular(12),
+        color: const Color(0xFF14181B),
+        borderRadius: BorderRadius.circular(8),
         border: Border.all(
-          color: Colors.black,
-          width: 2,
+          color: const Color(0xFF2E3741).withOpacity(0.8),
+          width: 1.2,
         ),
       ),
       child: Column(
@@ -26,7 +26,8 @@ class RightPanel extends StatelessWidget {
         children: [
           const Text(
             'Settings',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
           ),
           const SizedBox(height: 20),
           if (selectedComponent != null) ...[
@@ -35,21 +36,53 @@ class RightPanel extends StatelessWidget {
               style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
             ),
             const SizedBox(height: 20),
-            const Text('Text Settings', style: TextStyle(fontSize: 14)),
+            const Text('Text Settings', style: TextStyle(fontSize: 14, color: Color(0xFF8E8E93))),
             const SizedBox(height: 10),
             TextFormField(
               initialValue: "Sample Text",
-              decoration: const InputDecoration(labelText: 'Text'),
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(
+                  color: Colors.white,
+                ),
+                filled: true,
+                fillColor: const Color(0xFF2E3741).withOpacity(0.8),
+                contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(
+                    color: Colors.grey[400]!,
+                    width: 1.5,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(
+                    color: Colors.grey[400]!,
+                    width: 1.5,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Color(0xFF8E8E93),
+                    width: 2,
+                  ),
+                ),
+              ),
               onChanged: (value) {
-                BlocProvider.of<LogicBloc>(context)
-                    .add(UpdateComponentTextEvent(
-                  componentIndex: 0, // index of the component in the droppedComponents list
-                  newText: "New Text",
+                BlocProvider.of<LogicBloc>(context).add(UpdateComponentTextEvent(
+                  componentIndex: 0,
+                  newText: value,
                 ));
               },
             ),
             const SizedBox(height: 20),
-            const Text('Color Settings', style: TextStyle(fontSize: 14)),
+            const Text('Color Settings',
+                style: TextStyle(fontSize: 14, color: Colors.white)),
             const SizedBox(height: 10),
             DropdownButton<Color>(
               value: Colors.black,
@@ -59,14 +92,17 @@ class RightPanel extends StatelessWidget {
                 DropdownMenuItem(value: Colors.red, child: Text('Red')),
               ],
               onChanged: (Color? newColor) {
-                BlocProvider.of<LogicBloc>(context).add(UpdateComponentColorEvent(
-                  componentIndex: 0, // index of the component in the droppedComponents list
+                BlocProvider.of<LogicBloc>(context)
+                    .add(UpdateComponentColorEvent(
+                  componentIndex:
+                      0, // index of the component in the droppedComponents list
                   newColor: Colors.red,
                 ));
               },
             ),
             const SizedBox(height: 20),
-            const Text('Size Settings', style: TextStyle(fontSize: 14)),
+            const Text('Size Settings',
+                style: TextStyle(fontSize: 14, color: Colors.white)),
             const SizedBox(height: 10),
             Slider(
               value: 100,
@@ -75,8 +111,10 @@ class RightPanel extends StatelessWidget {
               divisions: 10,
               label: 'Size',
               onChanged: (double newSize) {
-                BlocProvider.of<LogicBloc>(context).add(UpdateComponentSizeEvent(
-                  componentIndex: 0, // index of the component in the droppedComponents list
+                BlocProvider.of<LogicBloc>(context)
+                    .add(UpdateComponentSizeEvent(
+                  componentIndex:
+                      0, // index of the component in the droppedComponents list
                   newSize: Size(200, 200),
                 ));
               },
@@ -84,7 +122,10 @@ class RightPanel extends StatelessWidget {
           ] else ...[
             const Text(
               'No component selected',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white),
             ),
           ]
         ],
